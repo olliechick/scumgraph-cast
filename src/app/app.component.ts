de67984d8868bef3fbf5d59c48f8549cde38e2ea
Ollie Chick <och26@uclive.ac.nz>
2020-05-07 18:39:22 +1200

    Move main page into player-selection component, and add chart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, NgZone, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,89 +7,22 @@ import {Component, NgZone, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  players: { name: string, colour: string }[] = [];
-  rows: { name: string, colour: string }[][] = [];
-
-  constructor(private ngZone: NgZone) {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
-    this.updateRows();
     const context = cast.framework.CastReceiverContext.getInstance();
-
-    function padAndChop(str, padChar, length) {
-      return (Array(length).fill(padChar).join('') + str).slice(length * -1);
-    }
-
-    function decimalToAARRGGBBHexTwosComplement(value) {
-      let binaryStr;
-      const bitCount = 8 * 4;
-
-      if (value >= 0) {
-        const twosComp = value.toString(2);
-        binaryStr = padAndChop(twosComp, '0', (bitCount || twosComp.length));
-      } else {
-        binaryStr = (Math.pow(2, bitCount) + value).toString(2);
-
-        if (Number(binaryStr) < 0) {
-          return undefined;
-        }
-      }
-
-      return '#' + parseInt(binaryStr, 2).toString(16).slice(2);
-    }
 
     const PLAYER_LIST_CHANNEL = 'urn:x-cast:nz.co.olliechick.scumgraph.playerlist';
     context.addCustomMessageListener(PLAYER_LIST_CHANNEL, customEvent => {
-      const players = customEvent.data.players;
-      players.forEach(item => item.colour = decimalToAARRGGBBHexTwosComplement(item.colour));
-      this.ngZone.run(() => this.players = players);
-      this.updateRows();
+      this.router.navigate(['/player-selection']);
     });
+
+    const CHART_CHANNEL = 'urn:x-cast:nz.co.olliechick.scumgraph.chart';
+    context.addCustomMessageListener(CHART_CHANNEL, customEvent => {
+      this.router.navigate(['/chart']);
+    });
+
     context.start();
-  }
-
-  addPlayer() { // for debugging purposes only
-    // this.players.push({name: 'New player', colour: '#ffbfa9'});
-    const players = [
-      {name: 'test', colour: '#f298ff'},
-      {name: '', colour: '#2a2a2a'},
-      {name: '', colour: '#2a2a2a'},
-      {name: '', colour: '#2a2a2a'}
-    ];
-    this.ngZone.run(() => this.players = players);
-
-    this.updateRows();
-  }
-
-  updateRows() {
-    const emptyRow = [
-      {name: '', colour: '#2a2a2a'},
-      {name: '', colour: '#2a2a2a'},
-      {name: '', colour: '#2a2a2a'},
-      {name: '', colour: '#2a2a2a'}
-    ];
-
-    const rows: { name: string, colour: string }[][] = [[...emptyRow]];
-    for (let i = 0; i < 3; i++) {
-      rows.push([...emptyRow]);
-    }
-
-    this.players.forEach((player, i) => {
-      rows[Math.floor(i / 4)][i % 4] = player;
-    });
-
-    this.ngZone.run(() => this.rows = rows);
-  }
-
-  getTextColour(colour: string) {
-    const red = parseInt(colour.slice(1, 3), 16);
-    const green = parseInt(colour.slice(3, 5), 16);
-    const blue = parseInt(colour.slice(5, 7), 16);
-
-    // https://en.wikipedia.org/wiki/YIQ
-    // https://24ways.org/2010/calculating-color-contrast/
-    const yiq = (red * 299 + green * 587 + blue * 114) / 1000;
-    return yiq < 192 ? 'white' : 'black';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material';
+import {ChartComponent} from './chart/chart.component';
+import {PlayerSelectionComponent} from './player-selection/player-selection.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'player-selection', component: PlayerSelectionComponent},
+  {path: 'chart', component: ChartComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    PlayerSelectionComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
